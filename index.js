b53adc706d1aeb0125b8e181c60edb1df0ad2835
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-05-06 02:17:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+};
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const rList = document.querySelectorAll('.ranked-list');
+
+  for (let i = 0, p = rList.length; i < p; i++) {
+    let children = rList[i].children;
+
+    for (let m = 0, j = children.length; m < j; m++) {
+        children[m].innerHTML = parseInt(children[m].innerHTML) + n;
+    }
+  }
+}
